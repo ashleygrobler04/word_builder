09eacd67b211a76b2bb3427c69324b1a199f93a3
ashleygrobler04 <ashleygrobler04@gmail.com>
2023-05-03 21:15:40 +0200

    First release

---
+++ script.js
@@ -0,0 +1,71 @@
+const lettersDiv = document.getElementById("letters");
+const wordsDiv = document.getElementById("words");
+const deleteBtn = document.getElementById("delete");
+const words = [
+  "test",
+  "train",
+  "drink",
+  "pray",
+  "water",
+  "ice",
+  "people",
+  "school",
+  "computer",
+  "title",
+];
+
+//let's get a random word from the words array:
+
+function getWord() {
+  let randomNum = Math.floor(Math.random() * words.length);
+  return words[randomNum];
+}
+
+//A function to shuffle given array.
+
+function shuffleArray(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    let randomNum = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[randomNum]] = [arr[randomNum], arr[i]];
+  }
+  return arr;
+}
+
+//get random word
+const chosenWord = getWord();
+//turn the chosen word into an array
+const wordArr = chosenWord.split("");
+//shuffle the word
+shuffleArray(wordArr);
+//let's turn each letter into a button that the user can click
+
+wordArr.forEach((v) => {
+  const letterBtn = document.createElement("button");
+  letterBtn.textContent = v;
+  letterBtn.addEventListener("click", (e) => {
+    wordsDiv.textContent += e.target.textContent;
+    checkWord();
+  });
+  lettersDiv.appendChild(letterBtn);
+});
+
+//Let's make the delete button do it's job
+
+deleteBtn.addEventListener("click", (e) => {
+  //split it's text content into an array to simply pop the last letter from the array
+  let text = wordsDiv.textContent.split("");
+  if (text.length > 0) {
+    text.pop();
+    //update the div's text content
+    wordsDiv.textContent = text.join("");
+  }
+});
+
+//let's write a function to check if the word the player entered matches the chosen word
+
+function checkWord() {
+  if (wordsDiv.textContent.toString() == chosenWord) {
+    alert("Well done!\nYou got the word and it was " + chosenWord);
+  }
+  console.log(wordsDiv.textContent == chosenWord);
+}
